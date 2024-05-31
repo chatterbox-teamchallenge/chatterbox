6896e234431e260125df57698a4c5cd34a3f9dd3
Halina Tulchii <halendeihalyna@gmail.com>
2024-05-31 15:34:17 +0300

    summary

---
+++ client/src/index.tsx
@@ -0,0 +1,16 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import "./styles/main.scss";
+// import reportWebVitals from "./reportWebVitals";
+// import { RouterProvider } from "react-router-dom";
+// import router from "./routes/router";
+
+// const root = ReactDOM.createRoot(
+//   document.getElementById("root") as HTMLElement
+// );
+// root.render(
+//   <React.StrictMode>
+//     <RouterProvider router={router} />
+//   </React.StrictMode>
+// );
+// reportWebVitals();
