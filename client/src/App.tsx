81574a903b48c4854f3137677e94be703bd13c80
Nikitich <elderlywitchy@gmail.com>
2024-06-01 14:15:15 +0300

    transfered code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import './styles/main.scss';
-// import Start from './pages/Start/Start';
+import Start from './pages/Start/Start';
 
 function App() {
   return (
     <div className="App">
-      {/* <Start/> */}
+      <Start/>
     </div>
   );
 }
---
+++ client/src/routes/router.tsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+import Start from "../pages/Start/Start";
+import Login from "../pages/Start/Login";
+// import Login from "../pages/Login";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Start />,
+  },
+  {
+    path: "register",
+    element: <Login type="register"/>,
+  },
+  {
+    path: "login",
+    element: <Login type="login"/>,
+  },
+]);
+
+export default router;
