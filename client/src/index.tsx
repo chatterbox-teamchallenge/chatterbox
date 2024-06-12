f9f2eaf4e8cc26c803bbe50d2d1a18e14915e0b5
Halina Tulchii <halendeihalyna@gmail.com>
2024-06-12 18:21:13 +0300

    commented some component  for deploy

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/main.scss";
-import App from "./App";
+// import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/router";
