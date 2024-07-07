f6ed084687436c9a35d904501a7a993d031f00a0
Arthur Kozub <kozub0246@gmail.com>
2024-07-07 15:57:56 +0300

    chore: update docs

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,8 +1,7 @@
-import { Body, Controller, Post, Get, Query } from '@nestjs/common';
+import { Body, Controller, Post, } from '@nestjs/common';
 import {  FinalizeSignupDto, LoginResponseDto, UserDto } from 'src/users/dto/userDto';
 import { AuthService } from './auth.service';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { User } from '@prisma/client';
 
 @ApiTags('Authorization')
 @Controller('auth')
@@ -11,21 +10,21 @@ export class AuthController {
     constructor(private authService: AuthService) { }
 
     @ApiOperation({ summary: 'Login', description: 'Login with username OR email' })
-    @ApiResponse({ status: 200, description: 'JWT Token', type: LoginResponseDto })
+    @ApiResponse({ status: 200, description: 'JWT Token along with user object' })
     @Post('/login')
     login(@Body() userDto: UserDto) {
         return this.authService.login(userDto);
     }
 
-    @ApiOperation({ summary: 'Signup', description: '' })
+    @ApiOperation({ summary: 'Signup', description: 'Signup with single email' })
     @ApiResponse({ status: 200, description: 'JWT Token', type: LoginResponseDto })
     @Post('/signup')
     signup(@Body() userDto: UserDto) {
         return this.authService.signup(userDto);
     }
 
-    @ApiOperation({ summary: 'Finalize confirmation and signup', description: 'Username is optional' })
-    @ApiResponse({ status: 200 })
+    @ApiOperation({ summary: 'Finalization of signup', description: 'Username is optional' })
+    @ApiResponse({ status: 200, description: 'JWT Token along with user object' })
     @Post('/finalize-signup')
     async finalizeSignup(@Body() finalizeSignupDto: FinalizeSignupDto) {
         return this.authService.finalizeSignup(finalizeSignupDto);
