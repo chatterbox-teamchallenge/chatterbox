48ecbea6389886266bccbb9ed023ff724f1e7d22
Arthur Kozub <kozub0246@gmail.com>
2024-07-09 12:54:38 +0300

    feat: forgot password feature, docs and logs included

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,13 +1,13 @@
 import { Controller, Post, Body, Get, UseGuards, Delete, Put } from '@nestjs/common';
 import { UserService } from './users.service';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiBadRequestResponse, ApiBody, ApiNotFoundResponse, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { User } from './users.model';
 
 @ApiTags('Users')
 @Controller('users')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) { }
 
   @ApiOperation({ summary: 'Get all users' })
   @ApiResponse({ status: 200, type: [User] })
@@ -17,6 +17,24 @@ export class UserController {
     return this.userService.getAllUsers();
   }
 
+  @ApiOperation({ summary: 'Forgot password', description: 'Username is optional' })
+  @ApiBadRequestResponse({ description: 'Bad Request' })
+  @ApiNotFoundResponse({ description: 'User not found' })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        newPassword: {
+          type: 'string',
+          example: 'newPassword123@#$%',
+        },
+        forgotPasswordToken: {
+          type: 'string',
+          example: '7b56fa4c-a134-46b4-a730-8debf0961ca6',
+        },
+      },
+    },
+  })
   @Put('/update-password')
   async updatePassword(@Body() body: { newPassword: string, forgotPasswordToken: string }) {
     return this.userService.updatePassword(body.newPassword, body.forgotPasswordToken)
