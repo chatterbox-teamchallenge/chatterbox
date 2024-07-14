e69e77c51823ddc01dfd7e7443385fb60efe735d
Arthur Kozub <kozub0246@gmail.com>
2024-07-14 15:04:49 +0300

    feat: add set user bio by userId

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Post, Body, Get, UseGuards, Delete, Put } from '@nestjs/common';
+import { Controller, Post, Body, Get, UseGuards, Delete, Put, Param, ParseIntPipe } from '@nestjs/common';
 import { UserService } from './users.service';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
-import { ApiBadRequestResponse, ApiBody, ApiNotFoundResponse, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiBadRequestResponse, ApiBody, ApiNotFoundResponse, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { User } from './users.model';
 
 @ApiTags('Users')
@@ -38,6 +38,35 @@ export class UserController {
   @Put('/update-password')
   async updatePassword(@Body() body: { newPassword: string, forgotPasswordToken: string }) {
     return this.userService.updatePassword(body.newPassword, body.forgotPasswordToken)
+  }
+
+  @ApiOperation({ summary: 'Set User bio', description: 'Set up/update user\'s bio' })
+  @ApiBadRequestResponse({ description: 'Bad Request' })
+  @ApiNotFoundResponse({ description: 'User not found' })
+  @ApiParam({
+    name: 'userId',
+    type: Number,
+    description: 'The ID of the user to set up the bio',
+    example: 1,
+    required: true,
+  })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        bio: {
+          type: 'string',
+          example: 'Yeah... That\'s my new bio',
+        }
+      },
+    },
+  })
+  @Put('set-bio/:userId')
+  async setUserBio(
+    @Param('userId', ParseIntPipe) userId: number,
+    @Body('bio') bio: string
+  ) {
+    return this.userService.setUserBio(userId, bio)
   }
 
   @ApiOperation({ summary: 'Delete user by email' })
